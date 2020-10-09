9eabb8bb93bb319d0c2d11c1bdf18ff6841e2847
neokii <neokii@gmail.com>
2020-10-10 01:07:37 +0900

    refactoring

--- app/src/main/java/com/neokii/ntune/IndiTuneActivity.kt
+++ app/src/main/java/com/neokii/ntune/IndiTuneActivity.kt
@@ -10,17 +10,17 @@ class IndiTuneActivity: BaseTuneActivity() {
 
     override fun getItemList(json: JSONObject): ArrayList<TuneItemInfo> {
 
-        return  ArrayList<TuneItemInfo>().also {
+        return  ArrayList<TuneItemInfo>().apply {
 
-            it.add(TuneItemInfo("innerLoopGain", json.getDouble("innerLoopGain").toFloat(), 0.5f, 10.0f, 0.05f, 3))
-            it.add(TuneItemInfo("outerLoopGain", json.getDouble("outerLoopGain").toFloat(), 0.5f, 10.0f, 0.05f, 3))
-            it.add(TuneItemInfo("timeConstant", json.getDouble("timeConstant").toFloat(), 0.1f, 5.0f, 0.05f, 3))
-            it.add(TuneItemInfo("actuatorEffectiveness", json.getDouble("actuatorEffectiveness").toFloat(), 0.1f, 5.0f, 0.05f, 3))
+            add(TuneItemInfo("innerLoopGain", json.getDouble("innerLoopGain").toFloat(), 0.5f, 10.0f, 0.05f, 3))
+            add(TuneItemInfo("outerLoopGain", json.getDouble("outerLoopGain").toFloat(), 0.5f, 10.0f, 0.05f, 3))
+            add(TuneItemInfo("timeConstant", json.getDouble("timeConstant").toFloat(), 0.1f, 5.0f, 0.05f, 3))
+            add(TuneItemInfo("actuatorEffectiveness", json.getDouble("actuatorEffectiveness").toFloat(), 0.1f, 5.0f, 0.05f, 3))
 
-            it.add(TuneItemInfo("steerLimitTimer", json.getDouble("steerLimitTimer").toFloat(),
-                0.5f, 3.0f, 0.05f, 3))
+            add(TuneItemInfo("steerLimitTimer", json.getDouble("steerLimitTimer").toFloat(),
+             0.5f, 3.0f, 0.05f, 3))
 
-            it.add(TuneItemInfo("steerMax", json.getDouble("steerMax").toFloat(),
+            add(TuneItemInfo("steerMax", json.getDouble("steerMax").toFloat(),
                 0.5f, 3.0f, 0.05f, 3))
         }
     }
--- app/src/main/java/com/neokii/ntune/LqrTuneActivity.kt
+++ app/src/main/java/com/neokii/ntune/LqrTuneActivity.kt
@@ -10,23 +10,23 @@ class LqrTuneActivity: BaseTuneActivity() {
 
     override fun getItemList(json: JSONObject): ArrayList<TuneItemInfo> {
 
-        return ArrayList<TuneItemInfo>().also {
+        return ArrayList<TuneItemInfo>().apply {
 
-            it.add(TuneItemInfo("scale", json.getDouble("scale").toFloat(), 500.0f, 5000.0f, 50.0f, 1))
-            it.add(TuneItemInfo("ki", json.getDouble("ki").toFloat(), 0.0f, 0.2f, 0.01f, 3))
+            add(TuneItemInfo("scale", json.getDouble("scale").toFloat(), 500.0f, 5000.0f, 50.0f, 1))
+            add(TuneItemInfo("ki", json.getDouble("ki").toFloat(), 0.0f, 0.2f, 0.01f, 3))
 
-            //it.add(TuneItemInfo("k_1", json.getDouble("k_1").toFloat(), -150.0f, -50.0f, 5.0f, 1))
-            //it.add(TuneItemInfo("k_2", json.getDouble("k_2").toFloat(), 400.0f, 500.0f, 5.0f, 1))
+            //add(TuneItemInfo("k_1", json.getDouble("k_1").toFloat(), -150.0f, -50.0f, 5.0f, 1))
+            //add(TuneItemInfo("k_2", json.getDouble("k_2").toFloat(), 400.0f, 500.0f, 5.0f, 1))
 
-            //it.add(TuneItemInfo("l_1", json.getDouble("l_1").toFloat(), 0.1f, 0.5f, 0.01f, 3))
-            //it.add(TuneItemInfo("l_2", json.getDouble("l_2").toFloat(), 0.1f, 0.5f, 0.01f, 3))
+            //add(TuneItemInfo("l_1", json.getDouble("l_1").toFloat(), 0.1f, 0.5f, 0.01f, 3))
+            //add(TuneItemInfo("l_2", json.getDouble("l_2").toFloat(), 0.1f, 0.5f, 0.01f, 3))
 
-            it.add(TuneItemInfo("dcGain", json.getDouble("dcGain").toFloat(), 0.0020f, 0.0040f, 0.0001f, 6))
+            add(TuneItemInfo("dcGain", json.getDouble("dcGain").toFloat(), 0.0020f, 0.0040f, 0.0001f, 6))
 
-            it.add(TuneItemInfo("steerLimitTimer", json.getDouble("steerLimitTimer").toFloat(),
+            add(TuneItemInfo("steerLimitTimer", json.getDouble("steerLimitTimer").toFloat(),
                 0.5f, 3.0f, 0.05f, 3))
 
-            it.add(TuneItemInfo("steerMax", json.getDouble("steerMax").toFloat(),
+            add(TuneItemInfo("steerMax", json.getDouble("steerMax").toFloat(),
                 0.5f, 3.0f, 0.05f, 3))
         }
     }
