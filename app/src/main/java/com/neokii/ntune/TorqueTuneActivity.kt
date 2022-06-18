23e94c4d469608dbb1cd9f0d9872e3ca24c72e7b
neokii <neokii@gmail.com>
2022-06-18 23:57:09 +0900

    1660

--- app/src/main/java/com/neokii/ntune/TorqueTuneActivity.kt
+++ app/src/main/java/com/neokii/ntune/TorqueTuneActivity.kt
@@ -20,7 +20,8 @@ class TorqueTuneActivity: BaseTuneActivity() {
             add(TuneItemInfo("friction", json.getDouble("friction").toFloat(), 0.0f, 0.2f, 0.01f, 3))
             add(TuneItemInfo("ki_factor", json.getDouble("ki_factor").toFloat(), 0.0f, 1.0f, 0.1f, 2))
             add(TuneItemInfo("kd", json.getDouble("kd").toFloat(), 0.0f, 2.0f, 0.1f, 2))
-            add(TuneItemInfo("deadzone", json.getDouble("deadzone").toFloat(), 0.0f, 0.05f, 0.01f, 3))
+            add(TuneItemInfo("angle_deadzone_v2", json.getDouble("angle_deadzone_v2").toFloat(),
+                0.0f, 2.0f, 0.01f, 2))
 
             add(TuneItemInfo("useSteeringAngle", json.getDouble("useSteeringAngle").toFloat(),
                 0f, 1.0f, 0f, 0))
