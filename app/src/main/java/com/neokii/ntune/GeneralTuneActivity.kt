edb81ad4001d4d0d73e494c985d5462b2fcfde2b
neokii <neokii@gmail.com>
2023-04-01 15:52:24 +0900

    1850

--- app/src/main/java/com/neokii/ntune/GeneralTuneActivity.kt
+++ app/src/main/java/com/neokii/ntune/GeneralTuneActivity.kt
@@ -19,6 +19,9 @@ class GeneralTuneActivity: BaseTuneActivity() {
         list.add(TuneItemInfo("pathOffset", json.getDouble("pathOffset").toFloat(),
             -1.0f, 1.0f, 0.01f, 2, R.string.tune_path_offset_desc))
 
+        list.add(TuneItemInfo("pathFactor", json.getDouble("pathFactor").toFloat(),
+            0.6f, 1.2f, 0.1f, 2))
+
         if(!Feature.FEATURE_UNIVERSAL)
         {
             list.add(TuneItemInfo("useLiveSteerRatio", json.getDouble("useLiveSteerRatio").toFloat(),
