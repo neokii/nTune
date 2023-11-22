d2aa92de7e5c563e565adf0bdfa64a6ffe9bc95c
neokii <neokii@gmail.com>
2023-11-22 21:08:38 +0900

    1870

--- app/src/main/java/com/neokii/ntune/GeneralTuneActivity.kt
+++ app/src/main/java/com/neokii/ntune/GeneralTuneActivity.kt
@@ -16,11 +16,11 @@ class GeneralTuneActivity: BaseTuneActivity() {
 
         val list = ArrayList<TuneItemInfo>()
 
-        list.add(TuneItemInfo("pathOffset", json.getDouble("pathOffset").toFloat(),
-            -1.0f, 1.0f, 0.01f, 2, R.string.tune_path_offset_desc))
+        list.add(TuneItemInfo("longLeadSensitivity", json.getDouble("longLeadSensitivity").toFloat(),
+            0.5f, 1.2f, 0.1f, 1, R.string.tune_long_lead_sensitivity_desc))
 
         list.add(TuneItemInfo("pathFactor", json.getDouble("pathFactor").toFloat(),
-            0.8f, 1.1f, 0.01f, 2, R.string.tune_path_factor_desc))
+            0.9f, 1.1f, 0.01f, 2, R.string.tune_path_factor_desc))
 
         if(!Feature.FEATURE_UNIVERSAL)
         {
